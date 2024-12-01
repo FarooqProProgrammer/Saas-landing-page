784de3e32e11420346c66d562b96bdceb3d4a41b
Muhammad Farooq <farooqayub646@gmail.com>
2024-12-01 03:19:07 -0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').pop();
+          if (/png|jpe?g|gif|svg|webp/.test(extType)) {
+            return 'assets/images/[name].[hash][extname]';
+          } else if (/css/.test(extType)) {
+            return 'assets/css/[name].[hash][extname]';
+          } else if (/woff2?|ttf|eot|otf/.test(extType)) {
+            return 'assets/fonts/[name].[hash][extname]';
+          }
+          return '[name].[hash][extname]';
+        }
+      }
+    }
+  }
 })
